410a48e01935b03de6ce31ce6ce781e8f760d0dd
redaelemawy <redaelemawy98@gmail.com>
2025-05-13 21:17:51 +0300

    fix (middleware): handle verification error.

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,14 +2,20 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { User } from '@prisma/client';
 const auth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
-  if (!token) {
+  const tokenString = req.headers.authorization;
+  if (!tokenString) {
     res.status(401).json({ message: 'Unauthorized' });
     return;
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-  req.user = decoded as User;
-  next();
+  try {
+    const token = tokenString?.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded as User;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: 'Unauthorized' });
+  }
 };
 
 export default auth;
