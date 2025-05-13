45e13d6e0767bcc68f36e1f92563f8dc7c9ffac1
redaelemawy <redaelemawy98@gmail.com>
2025-05-13 03:42:55 +0300

    feat: implement auth middlleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-import { AuthenticatedRequest } from '../types';
-
-const auth = async (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  // TODO: Implement authentication logic
+import jwt from 'jsonwebtoken';
+import { User } from '@prisma/client';
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+  req.user = decoded as User;
   next();
 };
 
