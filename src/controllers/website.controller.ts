def050d9e02d4ff065ac9dec81235a1b05956785
redaelemawy <redaelemawy98@gmail.com>
2025-05-15 15:54:59 +0300

    feat (website/create): add create website

--- src/controllers/website.controller.ts
+++ src/controllers/website.controller.ts
@@ -1,5 +1,12 @@
+import { WebsiteStatus } from '@prisma/client';
 import prisma from '../lib/prisma';
-
+import { Request, Response, NextFunction } from 'express';
+import { Tr } from '../types';
+import createFormidableParser, { getFirst } from '../lib/formidable-config';
+import { Fields, Files } from 'formidable';
+import path from 'path';
+import convertToSlug from '../utils/convertToSlug';
+import { saveFile } from '../utils/savFile';
 // Get all websites
 export const getWebsites = async (req: any, res: any) => {
   try {
@@ -14,4 +21,72 @@ export const getWebsites = async (req: any, res: any) => {
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
   }
+};
+
+interface CreateWebsiteBody {
+  name: string;
+  status: WebsiteStatus;
+  type: string;
+  topTitle: Tr<string>;
+  description: Tr<string>;
+  logo: {
+    name: string;
+    image: string;
+  };
+}
+
+// creat website
+export const createWebsite = async (req: Request, res: Response) => {
+  const form = createFormidableParser({ multiples: false });
+  form.parse(
+    req,
+    async (err: any, fields: Fields<'data'>, files: Files<'logoImage'>) => {
+      if (err) {
+        console.error('Form parse error:', err);
+        return res.status(400).send('Invalid form data');
+      }
+      let body = fields;
+      let bodyData = JSON.parse(
+        getFirst(body.data) || '{}'
+      ) as CreateWebsiteBody;
+      let logoImage = getFirst(files.logoImage);
+      try {
+        // store to database
+        // if not user throw error
+        if (!req.user) {
+          return res.status(401).json({ message: 'Unauthorized' });
+        }
+        console.log(bodyData);
+        const website = await prisma.website.create({
+          data: {
+            slug: convertToSlug(bodyData.name),
+            name: bodyData.name,
+            status: bodyData.status,
+            type: bodyData.type,
+            meta: {
+              topTitle: bodyData.topTitle,
+              description: bodyData.description,
+              logoImage: logoImage
+                ? path.join('uploads', logoImage.newFilename)
+                : null,
+            },
+            userId: req.user.id,
+          },
+        });
+        try {
+          // store file to storage
+          if (logoImage) {
+            await saveFile(logoImage);
+          }
+          return res.json({ message: 'Website created successfully', website });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({ message: 'Server error' });
+        }
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Server error' });
+      }
+    }
+  );
 };
--- src/routes/website.routes.ts
+++ src/routes/website.routes.ts
@@ -4,5 +4,5 @@ import * as WebsiteController from '../controllers/website.controller';
 const router = Router();
 
 router.get('/', auth, (req, res) => WebsiteController.getWebsites(req, res));
-
+router.post('/', auth, (req, res) => WebsiteController.createWebsite(req, res));
 export default router;
