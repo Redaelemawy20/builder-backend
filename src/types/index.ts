4671f069d3400113b21ea959cffab7f8a2f2f6ce
redaelemawy <redaelemawy98@gmail.com>
2025-05-15 04:21:50 +0300

    feat (ts): add Translatable type (Tr)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,49 +1,5 @@
-import { Request } from 'express';
-import {
-  User,
-  Cerdential,
-  Role,
-  Permission,
-  RolePermission,
-  Entity,
-  Page,
-  Section,
-  Layout,
-  PageSections,
-  News,
-} from '@prisma/client';
+export type Lang = 'en' | 'ar';
 
-export type SectionType = 'nav' | 'footer' | 'news' | 'section' | 'persons';
-
-export interface AuthenticatedRequest extends Request {
-  credential?: any;
-  user?: User;
-  role?: any;
-  file?: Express.Multer.File;
-}
-
-export interface UserWithRelations extends User {
-  entity?: Entity;
-  credential?: any;
-}
-
-export interface EntityWithRelations extends Entity {
-  pages: Page[];
-  news: News[];
-  staff: User[];
-  layoutItem: any[];
-}
-
-export interface PageWithRelations extends Page {
-  entity: Entity;
-  sections: any[];
-}
-
-export interface SectionWithRelations extends Section {
-  pages: any[];
-  layouts: any[];
-}
-
-export interface NewsWithRelations extends News {
-  entity: Entity;
-}
+export type Tr<T> = {
+  [key in Lang]: T;
+};
